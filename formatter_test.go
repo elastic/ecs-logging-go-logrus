b0f8346bc26d6f1dfa6eafd5b24429fdb87ab4aa
Andrew Wilkins <axw@elastic.co>
2021-01-14 16:14:20 +0800

    Add spec and validation tests (#6)
    Copy the common ECS logger spec, and add a test
    that validates logger output against it.

--- formatter_test.go
+++ formatter_test.go
@@ -19,27 +19,29 @@ package ecslogrus_test
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"runtime"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.elastic.co/ecslogrus"
+	"go.elastic.co/ecslogrus/internal/spec"
 )
 
 func TestFormatter(t *testing.T) {
 	var buf bytes.Buffer
-	log := logrus.New()
-	log.SetLevel(logrus.DebugLevel)
-	log.SetOutput(&buf)
-	log.SetFormatter(&ecslogrus.Formatter{
+	log := newLogger(&buf, &ecslogrus.Formatter{
 		DataKey:          "labels",
 		CallerPrettyfier: func(*runtime.Frame) (function string, file string) { return "function_name", "file_name" },
 	})
-	log.ReportCaller = true
 
 	epoch := time.Unix(0, 0).UTC()
 	err := errors.New("oy vey")
@@ -50,25 +52,107 @@ func TestFormatter(t *testing.T) {
 	)
 }
 
-func BenchmarkFormatter(b *testing.B) {
-	newLogger := func(formatter logrus.Formatter) *logrus.Logger {
-		log := logrus.New()
-		log.SetLevel(logrus.DebugLevel)
-		log.SetOutput(ioutil.Discard)
-		log.SetFormatter(formatter)
-		log.ReportCaller = true
-		return log
-	}
+func TestSpecValidation(t *testing.T) {
+	t.Run("minimal", func(t *testing.T) {
+		var buf bytes.Buffer
+		log := newLogger(&buf, &ecslogrus.Formatter{})
+		log.ReportCaller = false
+		log.Info()
 
+		var decoded map[string]interface{}
+		require.NoError(t, json.Unmarshal(buf.Bytes(), &decoded))
+		validateSpec(t, decoded, spec.V1)
+	})
+	t.Run("caller", func(t *testing.T) {
+		var buf bytes.Buffer
+		log := newLogger(&buf, &ecslogrus.Formatter{
+			CallerPrettyfier: func(*runtime.Frame) (function string, file string) { return "function_name", "file_name" },
+		})
+		log.Info()
+
+		var decoded map[string]interface{}
+		require.NoError(t, json.Unmarshal(buf.Bytes(), &decoded))
+		validateSpec(t, decoded, spec.V1)
+	})
+	t.Run("error", func(t *testing.T) {
+		var buf bytes.Buffer
+		log := newLogger(&buf, &ecslogrus.Formatter{})
+		log.WithError(errors.New("oy vey")).Error("oh noes")
+
+		var decoded map[string]interface{}
+		require.NoError(t, json.Unmarshal(buf.Bytes(), &decoded))
+		validateSpec(t, decoded, spec.V1)
+	})
+}
+
+func validateSpec(t *testing.T, fields map[string]interface{}, spec *spec.Spec, additionalFields ...string) {
+	flattened := make(map[string]interface{})
+	flattenMap(fields, flattened, "")
+	for name, field := range spec.Fields {
+		validateField(t, flattened, name, field)
+	}
+	for name := range flattened {
+		if _, ok := spec.Fields[name]; !ok {
+			assert.Contains(t, additionalFields, name, "unexpected field %q", name)
+		}
+	}
+}
+
+func validateField(t *testing.T, fields map[string]interface{}, name string, field spec.Field) {
+	val, ok := fields[name]
+	if field.Required { // all required fields must be present in the log line
+		require.True(t, ok)
+		require.NotNil(t, val)
+	} else if !ok {
+		return
+	}
+	if field.Type != "" { // the defined type must be met
+		var ok bool
+		switch field.Type {
+		case "string":
+			_, ok = val.(string)
+		case "datetime":
+			var s string
+			s, ok = val.(string)
+			if _, err := time.Parse("2006-01-02T15:04:05.000Z0700", s); err == nil {
+				ok = true
+			}
+		case "integer":
+			// json.Unmarshal unmarshals into float64 instead of int
+			if _, floatOK := val.(float64); floatOK {
+				_, err := strconv.ParseInt(fmt.Sprintf("%v", val), 10, 64)
+				if err == nil {
+					ok = true
+				}
+			}
+		default:
+			panic(fmt.Errorf("unhandled type %s from specification for field %s", field.Type, name))
+		}
+		require.True(t, ok, fmt.Sprintf("%s: %v", name, val))
+	}
+}
+
+func flattenMap(m, flattened map[string]interface{}, prefix string) {
+	for k, v := range m {
+		switch v := v.(type) {
+		case map[string]interface{}:
+			flattenMap(v, flattened, prefix+k+".")
+		default:
+			flattened[prefix+k] = v
+		}
+	}
+}
+
+func BenchmarkFormatter(b *testing.B) {
 	test := func(b *testing.B, f func(b *testing.B, logger *logrus.Logger)) {
 		b.Run("json", func(b *testing.B) {
-			f(b, newLogger(&logrus.JSONFormatter{
+			f(b, newLogger(ioutil.Discard, &logrus.JSONFormatter{
 				DataKey:          "labels",
 				CallerPrettyfier: func(*runtime.Frame) (function string, file string) { return "function_name", "file_name" },
 			}))
 		})
 		b.Run("ecs", func(b *testing.B) {
-			f(b, newLogger(&ecslogrus.Formatter{
+			f(b, newLogger(ioutil.Discard, &ecslogrus.Formatter{
 				DataKey:          "labels",
 				CallerPrettyfier: func(*runtime.Frame) (function string, file string) { return "function_name", "file_name" },
 			}))
@@ -81,4 +165,13 @@ func BenchmarkFormatter(b *testing.B) {
 			logger.WithError(err).WithField("custom", "field").Error("oh noes")
 		}
 	})
+}
+
+func newLogger(w io.Writer, formatter logrus.Formatter) *logrus.Logger {
+	log := logrus.New()
+	log.SetLevel(logrus.DebugLevel)
+	log.SetOutput(w)
+	log.SetFormatter(formatter)
+	log.ReportCaller = true
+	return log
 }
